70cbbbef2810bfd469d18d87e49ff6c367a0dc53
albertoivo <albertoivo@gmail.com>
2013-12-13 08:17:56 +0000

    Button e TextField

---
+++ impl/runner/robotium/src/main/java/br/gov/frameworkdemoiselle/behave/runner/robotium/ui/AndroidButton.java
@@ -0,0 +1,19 @@
+package br.gov.frameworkdemoiselle.behave.runner.robotium.ui;
+
+import br.gov.frameworkdemoiselle.behave.runner.ui.Button;
+
+public class AndroidButton extends AndroidBase implements Button {
+
+	@Override
+	public void click() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseOver() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ impl/runner/robotium/src/main/java/br/gov/frameworkdemoiselle/behave/runner/robotium/ui/AndroidTextField.java
@@ -0,0 +1,25 @@
+package br.gov.frameworkdemoiselle.behave.runner.robotium.ui;
+
+import br.gov.frameworkdemoiselle.behave.runner.ui.TextField;
+
+public class AndroidTextField extends AndroidBase implements TextField {
+
+	@Override
+	public void sendKeys(CharSequence... keysToSend) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendKeysWithTries(CharSequence... keysToSend) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
